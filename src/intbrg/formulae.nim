afefa45a5f9d11d07a17b158e115722e94d71951
Azumabashi <>
2024-06-20 15:39:10 +0900

    define logical operators

--- src/intbrg/formulae.nim
+++ src/intbrg/formulae.nim
@@ -1,1 +1,7 @@
 type Formulae* = distinct uint64 | distinct uint32 | distinct uint16 | distinct uint8
+
+# Logical operators
+proc `and`*(x, y: Formulae): Formulae = x and y
+proc `or`*(x, y: Formulae): Formulae = x or y
+proc `not`*(x: Formulae): Formulae = not x
+proc `implies`*(left, right: Formulae): Formulae = (not left) or right
