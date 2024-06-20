aa46b0b52a6f8cc1d9e4583d673398818c0cd775
Azumabashi <>
2024-06-20 15:50:17 +0900

    feat: define SAT-related functions

--- src/intbrg/models.nim
+++ src/intbrg/models.nim
@@ -6,3 +6,8 @@ func getModels*(f: Formulae): seq[int] =
   let size = 8 * sizeof(typeof(f))
   let str = $f
   return (0..<size).filterIt(str[i] == '1')
+
+# SAT-related functions
+func isSat*(f: Formulae): bool = f.getModels.len > 0
+func isUnsat*(f: Formulae): bool = not isSat(f)
+func isTautology*(f: Formulae): bool = f.getModels.len == 8 * sizeof(typeof(f))
