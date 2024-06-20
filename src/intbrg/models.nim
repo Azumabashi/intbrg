c7a57b059cb602b6db2553573f9bb88046409df5
Azumabashi <>
2024-06-20 20:11:04 +0900

    treats each models as binary string

--- src/intbrg/models.nim
+++ src/intbrg/models.nim
@@ -2,19 +2,28 @@ import formulae
 import sequtils
 import strutils
 
-type Model* = distinct uint
+type Model* = distinct string
 
 # test whether two models are the same or not
-func `==`*(x, y: Model): bool = x == y
+func `==`*(x, y: Model): bool {.borrow.}
 
 # stringify
-func `$`*(x: Model): string = toBin(x.int, 8)
+func `$`*(x: Model): string {.borrow.}
+
+func getDigits(truthTableCols: int): int =
+  var i = 0
+  while (1 shl i) != truthTableCols:
+    i += 1
+  return i
 
 func getModels*(f: Formulae): seq[Model] =
   ## Returns the set of f's models.
   let size = f.digits
   let str = $f
-  return (0..<size).toSeq.filterIt(str[it] == '1').mapIt(it.Model)
+  let digits = getDigits(size)
+  return (0..<size).toSeq
+    .filterIt(str[it] == '1')
+    .mapIt(toBin(size - it - 1, digits).Model)
 
 # SAT-related functions
 func isSat*(f: Formulae): bool = f.getModels.len > 0
