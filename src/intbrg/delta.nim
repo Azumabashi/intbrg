eaaf52e48eb330afd10810f78a3af5750f2ff517
Azumabashi <>
2024-06-20 17:59:53 +0900

    define R1 to R6

--- src/intbrg/delta.nim
+++ src/intbrg/delta.nim
@@ -21,3 +21,23 @@ func delta*(self: Formulae, others: seq[Formulae], dist: DistanceFunc, aggr: Agg
   for m in newModels:
     truthTable[m] = 1
   return truthTable.join("").parseBinInt().Formulae
+
+# R1 to R6
+func r1*(self: Formulae, others: seq[Formulae], dist: DistanceFunc, aggr: AggregateFunc): Formulae =
+  delta(getTautology(self.digits), others, dist, aggr)
+
+func r2*(self: Formulae, others: seq[Formulae], dist: DistanceFunc, aggr: AggregateFunc): Formulae =
+  let d = r1(self, others, dist, aggr)
+  delta(d, @[self], dist, aggr)
+
+func r3*(self: Formulae, others: seq[Formulae], dist: DistanceFunc, aggr: AggregateFunc): Formulae =
+  delta(getTautology(self.digits), @[self] + others, dist, aggr)
+
+func r4*(self: Formulae, others: seq[Formulae], dist: DistanceFunc, aggr: AggregateFunc): Formulae =
+  delta(getTautology(self.digits), @[self, r1(self, others, dist, aggr)], dist, aggr)
+
+func r5*(self: Formulae, others: seq[Formulae], dist: DistanceFunc, aggr: AggregateFunc): Formulae =
+  delta(self, r1(self, others, dist, aggr), dist, aggr)
+
+func r6*(self: Formulae, others: seq[Formulae], dist: DistanceFunc, aggr: AggregateFunc): Formulae =
+  delta(self, others, dist, aggr)
