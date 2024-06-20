a582b41a6f7445d1cbf420f5c57abe2c30d40434
Azumabashi <>
2024-06-20 15:41:02 +0900

    define stringify procedure

--- src/intbrg/formulae.nim
+++ src/intbrg/formulae.nim
@@ -5,3 +5,6 @@ proc `and`*(x, y: Formulae): Formulae = x and y
 proc `or`*(x, y: Formulae): Formulae = x or y
 proc `not`*(x: Formulae): Formulae = not x
 proc `implies`*(left, right: Formulae): Formulae = (not left) or right
+
+# stringify
+proc `$`*(x: Formulae): string = $x
